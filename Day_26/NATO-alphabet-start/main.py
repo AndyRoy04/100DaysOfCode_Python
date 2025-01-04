1daad9bf6238c6f167f0dca0573d0ded1b3a887e
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2025-01-04 12:51:41 -0500

    Updated NATO Alphabet code
    Added some try catch errors in order to deal with any errors

--- Day_26/NATO-alphabet-start/main.py
+++ Day_26/NATO-alphabet-start/main.py
@@ -9,6 +9,14 @@ phonetics_dict = {row.letter:row.code for index, row in phonetics_data.iterrows(
 # print(phonetics_dict)
 
 # #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input('Enter a word: ').upper()
-user_word_list = [phonetics_dict[letter] for letter in user_word if letter in phonetics_dict]
-print(user_word_list)
+def get_phonetic():
+    user_word = input('Enter a word: ').upper()
+    try:
+        user_word_list = [phonetics_dict[letter] for letter in user_word]
+    except KeyError:
+        print('The word contains non-alphabetical characters.')
+        get_phonetic()
+    else:
+        print(user_word_list)
+
+get_phonetic()
