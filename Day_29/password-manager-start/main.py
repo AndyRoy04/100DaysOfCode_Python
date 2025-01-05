61d2c7963a42610eefe8983e9e93ba0c59a19de1
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2025-01-05 14:41:39 -0500

    Update main.py
    Added some search functionality

--- Day_29/password-manager-start/main.py
+++ Day_29/password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 DARK = 'gray15'
 GRAY2 = 'gray27'
@@ -29,21 +30,53 @@ def generate_password():
     password_entry.insert(0, string_password)
     pyperclip.copy(string_password)
 
+# ---------------------------- SEARCG PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get().title()   # This line converts the website into title case format
+    
+    try:
+        with open('100DaysOfCode_Python/Day_29/password-manager-start/password_handler.json', 'r') as website_data:
+            data = json.load(website_data)
+    except FileNotFoundError:
+        messagebox.showerror(title='Error', message='No data file found')
+    else:
+        if len(website) == 0:
+            messagebox.showerror(title='Error', message='Please enter a website')
+        elif website in data:
+            messagebox.showinfo(title=website, message=f"Username: {data[website]['username']}\n"
+                                f"Password: {data[website]['password']}")
+            website_entry.delete(0, END)
+        else:
+            messagebox.showinfo(title=website, message='No details found for this website.')
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    data_dictionary = {
+        website.title(): {      # This line converts the website into title case format
+            'username': username,
+            'password': password
+        }
+    }
+    
     if website == '' or username == '' or password == '':
         messagebox.showinfo(title='Oops', message="Please don't leave any fields blank!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Are you sure you want to save"
-                                       f"\nEMAIL: {username}\nPASSWORD: {password}")
-        if is_ok:
-            with open('100DaysOfCode_Python/Day_29/password-manager-start/password_handler.docx', 'a') as data_entered:
-                data_entered.write(f"{website} | {username} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open('100DaysOfCode_Python/Day_29/password-manager-start/password_handler.json', 'r') as data_entered:
+                data = json.load(data_entered)  #  Loading the old data from the json file 
+                data.update(data_dictionary)    #  updating old json data with new data
+        except FileNotFoundError:
+            with open('100DaysOfCode_Python/Day_29/password-manager-start/password_handler.json', 'w') as data_entered:
+                json.dump(data_dictionary, data_entered, indent=4)  # saving updated data
+        else:
+            with open('100DaysOfCode_Python/Day_29/password-manager-start/password_handler.json', 'w') as data_entered:
+                json.dump(data, data_entered, indent=4, sort_keys=True)  # saving updated data
+        finally:                
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -60,10 +93,9 @@ canvas.grid(row=0, column=1)
 website_label = Label(text='Website: ', fg=LIGHT, bg=DARK, font=FONT)
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=43, fg=LIGHT, bg=GRAY2, borderwidth=0)
+website_entry = Entry(width=33, fg=LIGHT, bg=GRAY2, borderwidth=0)
 website_entry.focus()
-website_entry.place(height=15)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 
 # Username/Email label and entry
 username_label = Label(text='Email/Username: ', fg=LIGHT, bg=DARK, font=FONT)
@@ -79,6 +111,10 @@ password_label.grid(row=3, column=0)
 
 password_entry = Entry(width=33, fg=LIGHT, bg=GRAY2, borderwidth=0)
 password_entry.grid(row=3, column=1)
+
+# Search Button
+search_button = Button(text='Search', width=7, fg=LIGHT, bg=BLUE, command=find_password)
+search_button.grid(row=1, column=2)
 
 # Generate Password button
 generate_password_button = Button(text='Generate', fg=LIGHT, bg=BLUE, command=generate_password)
