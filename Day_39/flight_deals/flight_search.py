6b1bb283920e24ad76309b13b6a8dc70864ce8b6
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2025-01-14 23:33:43 -0500

    Create flight_search.py

---
+++ Day_39/flight_deals/flight_search.py
@@ -0,0 +1,8 @@
+import requests
+
+class FlightSearch:
+    #This class is responsible for talking to the Flight Search API.
+    def get_iata_code(self, iata):
+    #Function to get the IATA code from an airport name
+        city_code = "TESTING"
+        return city_code
