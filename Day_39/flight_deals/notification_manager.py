a100bdea8c44acbf023215c83650b4de22c7424b
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2025-01-17 17:49:31 -0500

    Update notification_manager.py
    Email message sending implemented

--- Day_39/flight_deals/notification_manager.py
+++ Day_39/flight_deals/notification_manager.py
@@ -1,5 +1,6 @@
-import requests
 import os
+import requests
+import smtplib
 from twilio.rest import Client
 from dotenv import load_dotenv
 
@@ -8,24 +9,34 @@ load_dotenv()
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    def send_email(self, current_price, price, a_airport, out_date, in_date):
-        """Function to send an email notification with the current price, price, arrival airport, and flight dates."""
+    def __init__(self):
         self._account_id = os.getenv("ACCOUNT_SID")
         self._auth_token = os.getenv("AUTH_TOKEN")
         self.sender_number = os.getenv("TWILIO_NUMBER")
         self.receiver_number = os.getenv("VERIFIED_NUMBER")
-        
+        self.my_mail = os.getenv("MY_MAIL")
+        self.secret_key = os.getenv("MY_PASSWORD")
+                
+    def send_message(self, current_price, price, a_airport, out_date, in_date, stops, carriers_name):
+        """Function to send a Whatsapp message with the current price, price, arrival airport, and flight dates."""
+        #Uncomment to send message to whatsapp
         if float(current_price) < float(price):
             client = Client(self._account_id, self._auth_token)
 
             message = client.messages.create(
             from_=f"whatsapp:{self.sender_number}",
-            body=f"Low price alert! only ${current_price} to fly from {DEPATURE_AIRPORT_CODE} to {a_airport},"
-            f"on {out_date} until {in_date}",
+            body=f"Low price alert! only ${current_price} to fly from {DEPATURE_AIRPORT_CODE} to {a_airport}, "
+            f"with only {stops} stop(s) ({carriers_name}) on {out_date} until {in_date}",
             to=f"whatsapp:{self.receiver_number}"
             )
             print(message.status)
-        
-        
-        
-    
+
+    def send_email(self, emails, current_price, price, a_airport, out_date, in_date, stops, carriers_name):  
+        """Function to send an email notification with the current price, price, arrival airport, and flight dates."""
+        if float(current_price) < float(price):
+            for email in emails:
+                with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+                    connection.starttls()
+                    connection.login(self.my_mail, self.secret_key)
+                    self.message = f"Only ${current_price} to fly from {DEPATURE_AIRPORT_CODE} to {a_airport}, with only {stops} stop(s) ({carriers_name}) on {out_date} until {in_date}"
+                    connection.sendmail(self.my_mail, email, msg=f'Subject: Low Price Alert!\n\n{self.message}')
