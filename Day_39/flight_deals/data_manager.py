4440b70737c76e2ece61a72c87b78710c7596434
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2025-01-17 17:54:02 -0500

    Update data_manager.py
    Retrieving customer email from google sheet

--- Day_39/flight_deals/data_manager.py
+++ Day_39/flight_deals/data_manager.py
@@ -10,16 +10,29 @@ class DataManager:
         self.end_log = os.getenv('ENDPOINT_LOG')
         self.auth_key = os.getenv('AUTHORIZATION_KEY')
         self.header = {'Authorization': f'{self.auth_key}'}
-        self.sheety_end_point = f'https://api.sheety.co/{self.end_log}/flightDeals/prices'
+        self.price_endpoint = f'https://api.sheety.co/{self.end_log}/flightDeals/prices'
+        self.user_endpoint = f'https://api.sheety.co/{self.end_log}/flightDeals/users'
+        self.user_list = []
 
     def get_data(self):
-        self.response = requests.get(self.sheety_end_point, headers=self.header)
+        # This method fetches price data from the Google Sheet.
+        self.response = requests.get(self.price_endpoint, headers=self.header)
         self.data = self.response.json()
         self.prices = self.data['prices']
         return self.prices
+    
+    def get_customer_emails(self):
+        # This method fetches user emails from the Google Sheet.
+        self.response = requests.get(self.user_endpoint, headers=self.header)
+        self.data = self.response.json()
+        self.users = self.data['users']
+        for user in self.users:
+            self.user_list.append(user['whatIsYourEmail?'])  # Extracting user emails for sending emails.
+        return self.user_list
 
     def update_data(self, row_id, iata_code):
-        SHEERTY_PUT_ENDPOINT = f"{self.sheety_end_point}/{row_id}"
+        # This method updates the iata_code field for a given city
+        SHEERTY_PUT_ENDPOINT = f"{self.price_endpoint}/{row_id}"
         data = {
             "price": {
                 "iataCode": iata_code,
